497ded84974d8e5cc692b2bfbe467495d4c02dbd
Eugeniy Miropolskiy <19inch@mail.ru>
2022-04-17 09:48:36 +0300

    del

--- ovpn-statatus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#sudo tail -n 100000 /var/log/openvpn/openvpn.log | grep "MULTI: primary virtual IP for" | ccze -A
-
-sudo tail -f /var/log/openvpn/openvpn-status.log | ccze -A
-
-
--- ovpn.log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "sudo tail -n 15000 /var/log/openvpn/openvpn.log | grep "MULTI: primary virtual IP for" | ccze -A"
-sudo tail -n 15000 /var/log/openvpn/openvpn.log | grep "MULTI: primary virtual IP for" | ccze -A
-
-
